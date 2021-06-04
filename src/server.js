74b73140218976a1e043d122fa3f12730448ece9
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-06-04 00:50:01 -0300

    Get de /produtos/{id}

--- src/server.js
+++ src/server.js
@@ -6,6 +6,15 @@ const app = express()
 app.get ('/produtos', async(req, res) =>{
     return res.status(200).json(produtos)
 })
+app.get ('/produtos/:id', async(req,res)=>{
+    const { id } = req.params
+    for (let produto of produtos){
+        if (produto.id == id){
+            return res.json([produto])
+        }
+    }
+})
+
 
 app.listen(3000, function() {
     console.log('Server is running')
