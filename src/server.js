cd56db4f241525967821e82e4fd8b02cafddb882
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-06-04 00:49:23 -0300

    Get de /produtos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require ('express')
-const json = require ('./database-simulador.json')
+const produtos = require ('./recursos/produtos.json')
+const departamentos = require ('./recursos/departamentos.json')
 const app = express()
+
+app.get ('/produtos', async(req, res) =>{
+    return res.status(200).json(produtos)
+})
 
 app.listen(3000, function() {
     console.log('Server is running')
