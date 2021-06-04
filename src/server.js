e991c68afb85913976ebead5353888a5179bbd4b
Filipeferaujo <83776344+Filipeferaujo@users.noreply.github.com>
2021-06-04 10:06:03 -0300

    adicionando dependencia body-parse e post e put

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require ('express')
-const produtos = require ('./recursos/produtos.json')
-const departamentos = require ('./recursos/departamentos.json')
+var produtos = require ('./recursos/produtos.json')
+var departamentos = require ('./recursos/departamentos.json')
+const bodyParser = require ('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get ('/produtos', async(req, res) =>{
     return res.status(200).json(produtos)
@@ -28,7 +31,36 @@ app.get ('/departamentos/:id', async(req,res)=>{
     }
 })
 
-
+app.post('/produtos', async(req,res)=>{
+    const resposta = req.body
+    console.log (produtos)
+    for (let i of resposta){
+        if (i.id == 0 || i.descricao === undefined|| i.preco === 0|| i.estoque === undefined|| i.disponivel != true && i.disponivel != false|| i.destaque != true && i.destaque != false || i.departamento_id == 0 )
+        {
+        return res.status(400)
+        }
+    }
+    produtos = produtos.concat(resposta);
+    return res.status(202).json(resposta)
+})
+app.put('/produtos/:id', async(req,res)=>{
+    const resposta = req.body
+    const { id } = req.params
+    for (let produto of produtos){
+        if (produto.id == id){
+            produto.id = resposta[0].id
+            produto.estoque = resposta[0].estoque
+            produto.disponivel = resposta[0].disponivel
+            produto.destaque = resposta[0].destaque
+            produto.departamento_id = resposta[0].departamento_id
+            return res.status(202).json(produto)
+        }
+        else{
+            continue
+        }
+    return res.status(400)
+}
+});
 app.listen(3000, function() {
     console.log('Server is running')
 });
